a78a2756f81c5517706db588b151af69f6bea6ef
marimofire <marimofire@gmail.com>
2011-12-24 12:20:17 +0900

    delete extra var

--- src/prac07.js
+++ src/prac07.js
@@ -1,9 +1,7 @@
 var prac = function (tasks) {
   var answer = 0
-    , last = 0
     , current = 0;
   tasks.sort(function(a, b) { b[1] - a[1] } );
-  last = tasks[tasks.length-1][1];
 
   for (var i = 0; i < tasks.length; i++) {
     var next = tasks[i];
@@ -13,15 +11,8 @@ var prac = function (tasks) {
       answer++;
     }
   }
-
   return answer;
 };
 
-var tasks = [
-  [1, 3],
-  [2, 5],
-  [4, 7],
-  [6, 9],
-  [8, 10]
-];
+var tasks = [[1, 3], [2, 5], [4, 7], [6, 9], [8, 10]];
 console.log(prac(tasks));
